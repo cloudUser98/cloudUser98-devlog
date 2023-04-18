7760f8c82f4e2352ef3e3882d15bd9530c4f697b
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-04-18 14:49:47 -0600

    pushState test 3

--- modules/browserHistory.js
+++ modules/browserHistory.js
@@ -1,5 +1,5 @@
 export default function logHistory() {
-    actual = window.location.href
+    let actual = window.location.href
     actual += "/new";
     console.log(window.history)
     window.history.pushState({}, "", actual)
