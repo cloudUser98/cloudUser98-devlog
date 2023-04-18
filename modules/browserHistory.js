04bd9db45c16ba2882704ab18d345acf50af155c
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-04-18 15:27:33 -0600

    pushState test 7

--- modules/browserHistory.js
+++ modules/browserHistory.js
@@ -1,10 +1,12 @@
 export default function logHistory() {
     window.history.pushState({}, "", "/curriculum");
     const root = document.getElementById("root");
-
     const curriculum = document.createElement("p");
 
-    curriculum.innerHTML("Mi curriculum");
+    console.log(root)
+    console.log(curriculum)
+
+    //curriculum.innerHTML("Mi curriculum");
 
     root.appendChild(curriculum);
 }
