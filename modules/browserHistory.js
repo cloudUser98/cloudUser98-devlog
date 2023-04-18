cf36860af4bad92151a59f9a803d765f69ca5209
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-04-18 14:38:56 -0600

    pushState test

--- modules/browserHistory.js
+++ modules/browserHistory.js
@@ -1,3 +1,4 @@
 export default function logHistory() {
     console.log(window.history)
+    window.history.pushState({}, "", "https://clouduser98.github.io/luis-escobedo/modules/browserHistory.js")
 }
