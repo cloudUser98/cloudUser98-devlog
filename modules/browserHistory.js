6a8a994ff27ceb6a5c3b85d621083db97c4553c0
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-04-21 10:47:27 -0600

    change method for string concatenation

--- modules/browserHistory.js
+++ modules/browserHistory.js
@@ -1,7 +1,7 @@
 export default function logHistory(url, page) {
 
     // Change of the browsers url without reloading the page
-    let location = "%s%s" % (window.location.href, url);
+    let location = window.location.href + url;
     window.history.pushState({}, "", location);
     
     // Executing the method that renders the page
