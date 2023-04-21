47f265c4834d4c7b03878611fa94e62fa768d971
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-04-21 09:59:39 -0600

    added project euler header

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import project_euler from "project_euler"
 
 const button = document.getElementById("getHistory")
 
-button.addEventListener("click", logHistory("/project_euler", project_euler))
+button.addEventListener("click", () => logHistory("/project_euler", project_euler))
--- modules/project_euler.js
+++ modules/project_euler.js
@@ -3,11 +3,14 @@ export default function project_euler() {
     // Getting the root element of the document
     const root = document.getElementById("root");
 
+    root.innerHTML = ""
+
     // Creating the new elemenets for the document
     const header = document.createElement("H1");
     const body = document.createElement("div");
     const text = document.createElement("p");
 
+    header.innerHTML = "PROJECT EULER"
     text.innerHTML = "Aqui existiran mis avances en el proyecto Euler"
 
     root.appendChild(header);
