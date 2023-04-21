92c0a792ceec731ac305b9e90a717ebfb2ec662d
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-04-21 09:54:39 -0600

    fixed innterhtml bug

--- modules/project_euler.js
+++ modules/project_euler.js
@@ -8,7 +8,7 @@ export default function project_euler() {
     const body = document.createElement("div");
     const text = document.createElement("p");
 
-    text.innerHTML("Aqui existiran mis avances en el proyecto Euler")
+    text.innerHTML = "Aqui existiran mis avances en el proyecto Euler"
 
     root.appendChild(header);
     root.appendChild(body);
