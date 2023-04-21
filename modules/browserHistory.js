c359e329a9a399c3939bbbf16fa737d3a4e3568c
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-04-21 10:40:02 -0600

    change on window history nows respects default url

--- modules/browserHistory.js
+++ modules/browserHistory.js
@@ -1,8 +1,9 @@
 export default function logHistory(url, page) {
 
     // Change of the browsers url without reloading the page
+    location = "%s%s" % (window.location.href, url);
     window.history.pushState({}, "", url);
     
     // Executing the method that renders the page
-    page()
+    page();
 }
--- modules/project_euler.js
+++ modules/project_euler.js
@@ -3,15 +3,15 @@ export default function project_euler() {
     // Getting the root element of the document
     const root = document.getElementById("root");
 
-    root.innerHTML = ""
+    root.innerHTML = "";
 
     // Creating the new elemenets for the document
     const header = document.createElement("h1");
     const body = document.createElement("div");
     const text = document.createElement("p");
 
-    header.innerHTML = "PROJECT EULER"
-    text.innerHTML = "Aqui existiran mis avances en el proyecto Euler"
+    header.innerHTML = "PROJECT EULER";
+    text.innerHTML = "Aqui existiran mis avances en el proyecto Euler";
 
     root.appendChild(header);
     root.appendChild(body);
