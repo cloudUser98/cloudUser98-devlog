2b23979a637a7893521039bb3f74806ed4f91121
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-04-21 09:38:36 -0600

    fixed map json

---
+++ modules/project_euler.js
@@ -0,0 +1,16 @@
+export default function project_euler() {
+
+    // Getting the root element of the document
+    const root = document.getElementById("root");
+
+    // Creating the new elemenets for the document
+    const header = document.createElement("H1");
+    const body = document.createElement("div");
+    const text = document.createElement("p");
+
+    text.innerHTML("Aqui existiran mis avances en el proyecto Euler")
+
+    root.appendChild(header);
+    root.appendChild(body);
+    body.appendChild(text);
+}
