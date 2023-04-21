c9435f430432b43992b023cb08c7335fe9b06f7c
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-04-21 13:05:06 -0600

    route defined for application

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ import project_euler from "project_euler"
 
 const button = document.getElementById("getHistory")
 
-button.addEventListener("click", () => logHistory("/project_euler", project_euler))
+const paths = window.location.pathname.split("/")
+const last_path = paths.pop()
+
+const router = {
+    "project_euler": project_euler
+}
+
+logHistory(last_path, project_euler[last_path]);
+
+button.addEventListener("click", () => logHistory("project_euler", project_euler))
