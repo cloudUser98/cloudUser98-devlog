d8d1c4eb3f1b89e1bbf19f3e16867f79a97a9ba1
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-04-24 16:15:48 -0600

    removed logs

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import Router from "history";
-import project_euler from "project_euler";
 
 const router = new Router();
 
@@ -10,7 +9,6 @@ const paths = window.location.pathname.split("/")
 let filtered_paths = paths.filter(path => path)
 const last_path = filtered_paths.pop()
 
-console.log("AAAAAAAAAAAAAAAAAAAAAAAAAA", last_path, router);
 // if (last_path in router) 
 //    logHistory(last_path, router[last_path]);
 
