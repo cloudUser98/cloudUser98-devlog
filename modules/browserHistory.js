681c23f26050734e60e228e77d88702d6981cade
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-04-24 16:23:45 -0600

    using path instead of whole location

--- modules/browserHistory.js
+++ modules/browserHistory.js
@@ -3,7 +3,7 @@ import project_euler from "project_euler";
 const load_route = (path, pageLoader) => {
     // Change of the browsers path without reloading the page
     let location = window.location.hostname + path;
-    window.history.pushState({}, "", location);
+    window.history.pushState({}, "", path);
     
     // Executing the method that renders the page
     pageLoader();
