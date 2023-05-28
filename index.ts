c0852eafb827f298a733f2ccafad56090e3859a6
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-05-27 22:37:17 -0600

    binded search bar to backslash

--- index.ts
+++ index.ts
@@ -3,15 +3,22 @@ import renderPatterns from "./modules/render-patterns";
 import { createRouter, route } from "simple-githubpages-spa-router";
 
 // NOTES:
-// Literlas: Constant values that are typed in the program as a part of the source code are called literals.
+// Literals: Constant values that are typed in the program as a part of the source code are called literals.
 // Literals can be of any of the basic data types and can be divided into Integer Numerals, Floating-Point Numerals, Characters, Strings, and Boolean Values.
 // Again, literals are treated just like regular variables except that their values cannot be modified after their definition.
 let router = createRouter([
     {path: "/study", pageRenderer: study},
     {path: "/render-patterns", pageRenderer: renderPatterns},
-],"/cloudUser98-devlog");
+],"/luis-escobedo");
 
 const systemThemeSwitch = document.getElementById("theme");
+const searchBar = document.getElementById("search-bar");
+
+window.onkeyup = (event) => {
+    if (event.key == "/") {
+        searchBar?.focus({preventScroll: false});
+    }
+};
 
 function setDarkTheme(ROOT_PSEUDOCLASS: HTMLElement) {
     ROOT_PSEUDOCLASS!.style.setProperty("--main-bg-color", "var(--bg-dark");
