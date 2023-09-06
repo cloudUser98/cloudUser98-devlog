a0f8e30aebde6c7fc0a46582a9438e0919f48dd8
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-09-06 17:04:38 -0600

    added darl/light icon for theme button, added projects html

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import study from "./modules/study";
 import renderPatterns from "./modules/render-patterns";
 import { createRouter } from "simple-githubpages-spa-router";
 
+var theme = false;
+
 // NOTES:
 // Literals: Constant values that are typed in the program as a part of the source code are called literals.
 // Literals can be of any of the basic data types and can be divided into Integer Numerals, Floating-Point Numerals, Characters, Strings, and Boolean Values.
@@ -9,9 +11,9 @@ import { createRouter } from "simple-githubpages-spa-router";
 let router = createRouter([
     {path: "/study", pageRenderer: study},
     {path: "/render-patterns", pageRenderer: renderPatterns},
-],"/cloudUser98-devlog");
+]);
 
-const systemThemeSwitch = document.getElementById("theme");
+const systemThemeSwitch = document.getElementById("theme2");
 const searchBar = document.getElementById("search-bar");
 
 window.onkeyup = (event) => {
@@ -34,19 +36,22 @@ function setLightTheme(ROOT_PSEUDOCLASS: HTMLElement) {
     ROOT_PSEUDOCLASS!.style.setProperty("--font-color", "var(--font-light");
 }
 
-function changeRootCustomProperties(e: Event) {
+function changeRootCustomProperties() {
+    theme = !theme;
 
     // Getting the root pseudoclass element
     const ROOT_PSEUDOCLASS = document.querySelector(":root") as HTMLElement | null;
 
-    if ((e.target as HTMLInputElement).checked) {
+    if (theme) {
         setDarkTheme(ROOT_PSEUDOCLASS!)
+        systemThemeSwitch!.classList.replace("fa-moon", "fa-sun");
     } else {
         setLightTheme(ROOT_PSEUDOCLASS!);
+        systemThemeSwitch!.classList.replace("fa-sun", "fa-moon");
     }
 }
 
 // Adding event listeners to the DOM elements
-systemThemeSwitch!.addEventListener("change", changeRootCustomProperties);
+systemThemeSwitch!.addEventListener("click", changeRootCustomProperties);
 
 // Change theme switch status according to the default prefered system theme
